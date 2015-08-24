85f636766066bd669c70cd10bfb022bddf482e1b
GGG KILLER <gggkiller2@gmail.com>
2015-08-24 18:03:41 -0300

    Initial commit

---
+++ TCM/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TCM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">verdade se for necessário descartar os recursos gerenciados; caso contrário, falso.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte do Designer - não modifique
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(65, 11);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(382, 20);
+            this.txtLogin.TabIndex = 1;
+            this.txtLogin.TextChanged += new System.EventHandler(this.txtLogin_TextChanged);
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(65, 44);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(382, 20);
+            this.txtSenha.TabIndex = 2;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Location = new System.Drawing.Point(14, 14);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(46, 13);
+            this.lblLogin.TabIndex = 3;
+            this.lblLogin.Text = "Usuário:";
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Location = new System.Drawing.Point(12, 46);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(41, 13);
+            this.lblSenha.TabIndex = 4;
+            this.lblSenha.Text = "Senha:";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(195, 70);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(104, 51);
+            this.btnGo.TabIndex = 5;
+            this.btnGo.Text = "Login";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(461, 138);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtLogin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResizeEnd += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
---
+++ TCM/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace TCM
+{
+    public partial class Form1 : Form
+    {
+        private ComponentResourceManager resources = new ComponentResourceManager(typeof(Form1));
+        private Image logo;
+        /// <summary>
+        /// Resizes an image
+        /// </summary>
+        /// <param name="imgToResize">Image to be resized</param>
+        /// <param name="size">Size to set the image to</param>
+        /// <returns></returns>
+        public static Image resizeImage(Image imgToResize, Size size)
+        {
+            return (Image)(new Bitmap(imgToResize, size));
+        }
+
+        /// <summary>  
+        /// method for changing the opacity of an image  
+        /// </summary>  
+        /// <param name="image">image to set opacity on</param>  
+        /// <param name="opacity">percentage of opacity</param>  
+        /// <returns></returns>  
+        public Image SetImageOpacity(Image image, double opacity)
+        {
+            MessageBox.Show(image.ToString());
+            try
+            {
+                //create a Bitmap the size of the image provided  
+                Bitmap bmp = new Bitmap(image.Width, image.Height);
+
+                //create a graphics object from the image  
+                using (Graphics gfx = Graphics.FromImage(bmp))
+                {
+
+                    //create a color matrix object  
+                    ColorMatrix matrix = new ColorMatrix();
+
+                    //set the opacity  
+                    matrix.Matrix33 = (float)opacity;
+
+                    //create image attributes  
+                    ImageAttributes attributes = new ImageAttributes();
+
+                    //set the color(opacity) of the image  
+                    attributes.SetColorMatrix(matrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+
+                    //now draw the image  
+                    gfx.DrawImage(image, new Rectangle(0, 0, bmp.Width, bmp.Height), 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, attributes);
+                }
+                return (Image)bmp;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n" + ex.StackTrace);
+                return null;
+            }
+        } 
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.White;
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            resizeBackground();
+        }
+
+        private void txtLogin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            
+                if ((txtLogin.Text == "Caiovigiani" ||
+                    txtLogin.Text == "Guilherme" ||
+                    txtLogin.Text == "Elisa" ||
+                    txtLogin.Text == "Gustavo" ||
+                    txtLogin.Text == "Mari") && txtSenha.Text == "12345") 
+                {
+                    this.Hide();
+                    (new Teste02(this)).Show();
+                    MessageBox.Show("Bem vindo " + txtLogin.Text + ".");
+                    txtLogin.Text = txtSenha.Text = "";
+                }
+                else 
+                {
+                    MessageBox.Show("what");
+                }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            resizeBackground();
+        }
+
+        private void resizeBackground()
+        {
+            if (logo == null)
+            {
+                logo = TCM.Properties.Resources.vetor_valt_boy; // Pega o logo do "Resources"
+                logo = SetImageOpacity(logo, 0.50); // Muda a transparência do logo
+            }
+            logo = resizeImage(logo, new Size(this.Width, this.Height)); // Resizes the image
+            this.BackgroundImage = logo; // Sets the logo as the background
+        }
+    }
+}
+
+
